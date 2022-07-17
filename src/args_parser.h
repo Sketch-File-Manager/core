2fa1d54aed3c05121ad1c72148fe13336da9c2f4
Constantinos Argyriou <rounnus@gmail.com>
2022-07-17 14:46:47 +0300

    Auto stash before merge of "main" and "origin/main"

--- src/args_parser.h
+++ src/args_parser.h
@@ -1,12 +1,10 @@
-#ifndef CORE_ARGS_PARSER_H
+I#ifndef CORE_ARGS_PARSER_H
 #define CORE_ARGS_PARSER_H
 
 #include <memory.h>
 #include <bits/types.h>
 
-struct args_parser_args {
-    unsigned int will_run: 1;
-
+struct args_parser_args { 
     unsigned int byte_rate; /* --byte-rate=516KB */
     char *byte_rate_measure;
     char *session_file; /* --session=session.txt */
@@ -14,11 +12,13 @@ struct args_parser_args {
     /**
      * mkdir, mkfile, copy, move, rename, perms, perms-recursive, delete, list
      * session-mkdir, session-mkfile, session-copy, session-move, session-rename, session-perms, session-perms-recursive, delete
-     * session-list, session-undo
+     * session-list, session-execute
      */
     char *command;
     char **command_argv;
     unsigned int command_argc;
+    unsigned int delete_session_after_execute: 1;
+    unsigned int will_run: 1;
 };
 
 static inline void init_args(struct args_parser_args *args) {
@@ -29,6 +29,6 @@ static inline void init_args(struct args_parser_args *args) {
  * Will parse the arguments that was passed when calling the program.
  * @param args The arguments struct.
  */
-extern void args_parser_parse(struct args_parser_args *args, int argc, char **argv);
+extern void args_parser_parse(struct args_parser_args *args, int argc, char *argv[]);
 
-#endif //CORE_ARGS_PARSER_H
+#endif
